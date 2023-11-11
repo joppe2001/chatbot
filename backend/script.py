ae20d80c0dbcb793a7fa117bfebb60553d523750
joppe montezinos <joppe.montezinos@gmail.com>
2023-11-11 18:19:05 +0100

    adding backend

---
+++ backend/script.py
@@ -0,0 +1,30 @@
+import openai
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Set your OpenAI API key
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+openai.api_key = os.getenv('OPEN_AI_KEY')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    try:
+        input_text = request.json.get('input')
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",  # or another appropriate model
+            messages=[{"role": "system", "content": "You are a helpful assistant."},
+                      {"role": "user", "content": input_text}]
+        )
+        return jsonify({'response': response['choices'][0]['message']['content']})
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({'response': 'Sorry, I am unable to respond right now.'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
